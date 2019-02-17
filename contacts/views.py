b664b7cdb8de50fcc4276deaa451bcc1de4ae93f
Dominik Grodzki <domort@users.noreply.github.com>
2019-02-17 21:40:42 +0000

    render contacts in a table1

--- contacts/views.py
+++ contacts/views.py
@@ -3,15 +3,22 @@ from __future__ import unicode_literals
 
 from django.http import Http404, JsonResponse
 from django.views.generic import TemplateView
+from django.template.loader import render_to_string
+
 from contacts.data.contact_store import ContactStore
 
 
 class ContactSearchView(TemplateView):
     template_name = 'contact-search.html'
+    contacts_template = 'contact-table.html'
     contact_store = ContactStore()
 
+    def render_table(self, contacts):
+        return render_to_string(self.contacts_template, {'contacts': contacts})
+
     def get(self, request, *args, **kwargs):
-        return self.render_to_response({})
+        contacts = self.contact_store.all_contacts
+        return self.render_to_response({'contact_table': self.render_table(contacts)})
 
     def post(self, request, *args, **kwargs):
         if not request.is_ajax():
