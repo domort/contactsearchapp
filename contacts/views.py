76e32c226544b6897f474bda724ef772bf959736
Dominik Grodzki <domort@users.noreply.github.com>
2019-02-17 20:53:43 +0000

    create contact-search html template

--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return HttpResponse("Hello, world")
+class ContactSearchView(TemplateView):
+    template_name = 'contact-search.html'
--- contactsearchapp/urls.py
+++ contactsearchapp/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
-from contacts.views import index
+from contacts.views import ContactSearchView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', index, name="index")
+    url(r'', ContactSearchView.as_view(), name="contact-search")
 ]
