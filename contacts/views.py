818e969ce707f51b17eb95ab74d065fd00e3f8de
Dominik Grodzki <domort@users.noreply.github.com>
2019-02-17 21:27:54 +0000

    send search term through ajax and receive it in the view

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import Http404, JsonResponse
 from django.views.generic import TemplateView
 from contacts.data.contact_store import ContactStore
 
@@ -9,5 +10,18 @@ class ContactSearchView(TemplateView):
     template_name = 'contact-search.html'
     contact_store = ContactStore()
 
-    def get(self, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return self.render_to_response({})
+
+    def post(self, request, *args, **kwargs):
+        if not request.is_ajax():
+            raise Http404()
+        else:
+            term = request.POST.get('term', None)
+            if term:
+                try:
+                    term = unicode(term)
+                except (TypeError, ValueError):
+                    term = None
+
+                return JsonResponse({})
